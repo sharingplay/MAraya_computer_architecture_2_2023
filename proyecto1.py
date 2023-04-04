a5a4673708f01228aa8586e89ab2ba3faf5d70e5
MAraya <marioaray1999@outlook.com>
2023-04-04 02:08:44 -0600

    creation of random functions and adds them to the log

--- proyecto1.py
+++ proyecto1.py
@@ -1,6 +1,7 @@
 import time
 import tkinter as tk
 import random
+import secrets
 import math
 
 class Memory:
@@ -26,7 +27,7 @@ class Processor:
     def __init__(self, number):
         self.logs = "" #registers every action the processor has done
         self.number = number
-        self.currentOperation = "Just got initialized"
+        self.currentOperation = []
         self.cache = {"000": 0,
                       "001": 0,
                       "010": 0,
@@ -36,14 +37,39 @@ class Processor:
                             "010": 'I',
                             "011": 'I'}
 
-    def updateLogs(self,newLog):
-        self.logs += "\n" + newLog
+    # assigns possible instructions to a processor
+    def getRandomInstruction(self):
+        def randomHex():  # return a random hex number of 16 bits or 4 bytes
+            return secrets.token_hex(2)
+
+        def randomBin():  # returns random binary of 3 digits
+            random.seed()
+            return bin(random.getrandbits(3))[2:].zfill(3)
+
+        # random number to get probability
+        randomNumber = random.uniform(0.1,1)
+
+        # 30% chance of getting a write instruction
+        if randomNumber < 0.3:
+            # return the processor number, the block number in binary and the hex value to write
+            self.currentOperation = ["P" + str(self.number),"WRITE",randomBin(),randomHex()]
+            self.logs += "\n" + str(self.currentOperation)
+            print(self.logs)
+        # 40% chance of getting a read instruction
+        elif randomNumber < 0.7:
+            # return the processor number and the block that is going to read
+            self.currentOperation = ["P" + str(self.number),"READ",randomBin()]
+            self.logs += "\n" + str(self.currentOperation)
+            print(self.logs)
+        # 30% chance of getting a calc instruction
+        else:
+            self.currentOperation = ["P" + str(self.number),"CALC"]
+            self.logs += "\n" + str(self.currentOperation)
+            print(self.logs)
+
 
     def getlogs(self):
         return self.logs
-
-    def updateCurrentOperation(self, currentOperation):
-        self.currentOperation = currentOperation
 
     def getCurrentOperation(self):
         return self.currentOperation
@@ -68,7 +94,7 @@ class Processor:
         return self.number
 
 class Ventana:
-    def __init__(self, master, mode):
+    def __init__(self, master, mode, listaProcesadores):
         self.currentOperations = {1: "",
                                   2: "",
                                   3: "",
@@ -78,16 +104,16 @@ class Ventana:
         self.update_id = None
 
         # creates 4 text boxes for the processors
-        self.text_box1 = tk.Text(self.master, height=10, width=30)
+        self.text_box1 = tk.Text(self.master, height=11, width=30)
         self.text_box1.grid(row=0, column=0, padx=10, pady=10)
 
-        self.text_box2 = tk.Text(self.master, height=10, width=30)
+        self.text_box2 = tk.Text(self.master, height=11, width=30)
         self.text_box2.grid(row=0, column=1, padx=10, pady=10)
 
-        self.text_box3 = tk.Text(self.master, height=10, width=30)
+        self.text_box3 = tk.Text(self.master, height=11, width=30)
         self.text_box3.grid(row=1, column=0, padx=10, pady=10)
 
-        self.text_box4 = tk.Text(self.master, height=10, width=30)
+        self.text_box4 = tk.Text(self.master, height=11, width=30)
         self.text_box4.grid(row=1, column=1, padx=10, pady=10)
 
         # creates a text box to display the memory data
@@ -99,7 +125,7 @@ class Ventana:
         self.text_box_info.grid(row=1, column=2, padx=10, pady=10)
 
         # creates the buttons
-        self.boton1 = tk.Button(self.master, text= "Contador")
+        self.boton1 = tk.Button(self.master, text= "accion", command=lambda: listaProcesadores[0].getRandomInstruction())
         self.boton1.grid(row=2, column=0, padx=10, pady=10)
 
         self.boton_pause = tk.Button(self.master, text="Pause")
@@ -110,6 +136,7 @@ class Ventana:
 
         self.boton4 = tk.Button(self.master, text="Boton 4")
         self.boton4.grid(row=2, column=3, padx=10, pady=10)
+
 
     # updates the window information
     def actualizar(self, lista_procesadores, memoria):
@@ -129,32 +156,36 @@ class Ventana:
             textBox.delete(1.0, tk.END)
 
             #Updates cache data for each processor
-            textBox.insert('end', f"Cache of processor {lista_procesadores[i].getNumber()}:\n")
-            for key,value in lista_procesadores[i].getCache().items(): #Cache values
+            textBox.tag_configure("center", justify="center",font=("Helvetica", 12, "bold"))
+            textBox.insert('end', f"Cache of processor {lista_procesadores[i].getNumber()}:\n","center")
+            for key,value in procesador.getCache().items(): #Cache values
                 textBox.insert('end', f"{key}: {value}\n")
 
-            textBox.insert('end', f"State of the cache blocks:\n")
+            textBox.tag_configure("center", justify="center",font=("Helvetica", 12, "bold"))
+            textBox.insert('end', f"State of the cache blocks:\n","center")
             for key,value in lista_procesadores[i].getCacheStates().items(): #Cache states
                 textBox.insert('end', f"{key}: {value}\n")
 
             #Keeps the operations done by the processors updated
             self.currentOperations[i+1] = lista_procesadores[i].getCurrentOperation()
 
-        #Writes the actions done by the processors
-        self.text_box_info.delete(1.0,tk.END)
-        for key, value in self.currentOperations.items():  #Cache values
-            self.text_box_info.insert(tk.END, f"Processor {key} action:\n{value}\n\n")
+            #Writes the actions done by the processors
+            self.text_box_info.delete(1.0,tk.END)
+            for key, value in self.currentOperations.items():  #Cache values
+                self.text_box_info.insert(tk.END, f"Processor {key} action:\n{value}\n\n")
+
 
         #Updates de memory on screen
         self.text_box_memory.delete(1.0, tk.END)
-        self.text_box_memory.insert(tk.END, "Shared Memory Blocks:\n")
+        self.text_box_memory.tag_configure("center", justify="center", font=("Helvetica", 12, "bold"))
+        self.text_box_memory.insert(tk.END, "Shared Memory Blocks:\n","center")
         for key, value in memoria.getMemBlocks().items():  # Cache values
             self.text_box_memory.insert(tk.END, f"Block {key} value:{value}\n\n")
 
-    def continuousUpdate(self, lista_procesadores, memoria):
+    def continuousUpdate(self, lista_procesadores, memoria, current_processor):
         # llama al método actualizar cada 2 segundos
         self.actualizar(lista_procesadores, memoria)
-        self.master.after(2000, lambda: self.continuousUpdate(lista_procesadores, memoria))
+        self.master.after(2000, lambda: self.continuousUpdate(lista_procesadores, memoria, current_processor))
 
         # inicia el bucle de eventos
         self.master.mainloop()
@@ -164,25 +195,20 @@ class Ventana:
         self.contador += 1
         print(self.contador)
 
-
-#assigns possible
-def getRandomInstruction():
-    randomNumber = random()
-
-    if randomNumber < 0.3:
-        return "write"
-    elif randomNumber < 0.7:
-        return "read"
-    else:
-        return "calc"
+    def set_lista_procesadores_memoria(self, lista_procesadores, memoria):
+        self.lista_procesadores = lista_procesadores
+        self.memoria = memoria
 
 
 def main():
     #validates execution mode
+    """
     try:
         mode = int(input("Seleccion el modo de ejecucion inicial:\n1)Ejecucion continua\n2)Ejecucion paso a paso\n"))
     except ValueError:
         print("El valor ingresado no es un entero")
+    """
+    mode = 1
     #Creates a memory
     mem = Memory()
 
@@ -198,12 +224,12 @@ def main():
     #Creates display window
     root = tk.Tk()
     root.title("Protocolo MOESI para coherencia de cache en sistemas multiprocesador")
-    ventana = Ventana(root,mode)
+    ventana = Ventana(root,mode,processors)
 
 
     #Updates display window
     if mode == 1:
-        ventana.continuousUpdate(processors,mem)
+        ventana.continuousUpdate(processors,mem,p1)
 
     elif mode == 2:
         ventana.stepUpdate(processors,mem)
@@ -211,6 +237,16 @@ def main():
 
     # ejecuta la ventana
     root.mainloop()
+    """
+    p1.getRandomInstruction()
+    p1.getRandomInstruction()
+    p1.getRandomInstruction()
+    p1.getRandomInstruction()
+    p1.getRandomInstruction()
+    p1.getRandomInstruction()
+    p1.getRandomInstruction()
+    p1.getRandomInstruction()
+"""
 
 if __name__ == "__main__":
     main()
