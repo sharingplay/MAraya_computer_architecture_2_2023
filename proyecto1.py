d5fa6db51dd04224c8ffe8199c555c3b9c65ea20
MAraya <marioaray1999@outlook.com>
2023-04-04 20:53:13 -0600

    2 way set validations not working

--- proyecto1.py
+++ proyecto1.py
@@ -26,6 +26,7 @@ class Memory:
 class Processor:
     def __init__(self, number):
         self.logs = "" #registers every action the processor has done
+        self.hitMiss = ""
         self.number = number
         self.currentOperation = []
         self.cache = {"000": 0,
@@ -67,21 +68,43 @@ class Processor:
             self.logs += "\n" + str(self.currentOperation)
             print(self.logs)
 
-
     def getlogs(self):
         return self.logs
 
     def getCurrentOperation(self):
         return self.currentOperation
 
-    def updateCache(self,cacheBlock,value,state):
-        self.cache[cacheBlock] = value
-        self.cacheStates[cacheBlock] = state
+
+    def updateCache(self,block,value,state): #validates the 2 way set in case a block needs to be changed
+        #If the block is in the cache, just overwrite it
+        if block in self.cache:
+            self.cache[block] = value
+            self.cacheStates[block] = state
+
+        #determines in which block it should be writen in case there is no invalid block ******************
+        else:
+            set = int(block) % 2 #gets the set with a 2 way set logic
+            if set == 0:
+                block1 = self.getCacheStates()[0]
+                block2 = self.getCacheStates()[1]
+                if "I" in block1:
+
+
+                for key, value in self.cacheStates.items():
+
+                if block1 == "I":
+                    self.cache[block] = self.cache.pop(block1)
+                elif block1 == "S":
+
+            else:
+                block3 = self.getCacheStates()[2]
+                block4 = self.getCacheStates()[3]
+
 
     def getCache(self):
         return self.cache
 
-    def getCacheBlock(self,cacheBlock):
+    def getCacheBlockValue(self,cacheBlock):
         return self.cache[cacheBlock]
 
     def getCacheStates(self):
@@ -92,6 +115,12 @@ class Processor:
 
     def getNumber(self):
         return self.number
+
+    def setHitMiss(self,state):
+        self.hitMiss = state
+
+    def getHitMiss(self):
+        return self.hitMiss
 
 class Ventana:
     def __init__(self, master, modo):
@@ -143,12 +172,62 @@ class Ventana:
         with self.lock: #adquire pause lock
             self.pause = not self.pause
 
+    def validateMOESI(self,instruction, listaProcesadores, memoria):
+        processorNumber = int(instruction[0][-1])-1
+        request = instruction[1]
+        processorCacheValues = listaProcesadores[processorNumber].getCache()
+        processorCacheStates = listaProcesadores[processorNumber].getCacheStates()
+
+        #handles exceptions for instructions that are not a write
+        try:
+            address = instruction[2]
+            value = instruction[3]
+        except IndexError:
+            address = ''
+            value = ''
+
+        if request == "READ":
+            for key, value in processorCacheStates.items():  # checks the cache states to see if it has to go to memory
+                print(f"Key {key} address: {address}")
+
+                #checks is the address is in the cache of the processor
+                if address == key:
+                    if value == "M" or "S" or "E":
+                        listaProcesadores[processorNumber].setHitMiss("Hit")
+                        print("Se hace un read en cache")
+                        print(f"valores cache: {processorCacheValues}")
+                        print(f"estados cache: {processorCacheStates}")
+                else:
+                    listaProcesadores[processorNumber].setHitMiss("Miss")
+                    print("Hay que leer los demas caches")
+
+                    #Checks if the value is valid in other processors
+                    for procesador in listaProcesadores:
+                        if address in procesador.getCacheStates() and procesador.getCacheState(address) != "I":
+                            #VALIDAR AQUI LOS DEMAS ESTADOS Y ACTUALIZAR EL CACHE **************
+                            print(f"El procesador {processorNumber} tenia el dato que buscaba")
+                        else:
+                            print(f"El procesador {processorNumber} no tiene la direccion que quiere leer o es Invalida")
+
+        #elif request == "WRITE":
+
+        #else: #Calc request
+
+        print("Validar MOESI")
+        print(f"instruccion: {instruction}")
+        print(f"procesadores: {listaProcesadores[0].getCache()}")
+        print(f"memoria: {memoria}")
+
+
     # updates the window information
     def actualizar(self, lista_procesadores, memoria):
         def newOperationRandProcessor(): # selects a random processor to give a random instruction
             pNumber = random.randint(0, 3)
             print(f"se asigna instruccion al procesador {pNumber+1}\n")
             lista_procesadores[pNumber].getRandomInstruction()
+
+            #calls the invalidation protocol
+            self.validateMOESI(lista_procesadores[pNumber].getCurrentOperation(),lista_procesadores,memoria)
 
 
         with self.lock:
